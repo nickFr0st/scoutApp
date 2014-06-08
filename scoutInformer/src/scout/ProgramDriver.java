b950b04aaeae943772e0584c46248bf0f9823d6b
Malloch <nathanaelmalloch@yahoo.com>
2014-06-07 22:41:22 -0600

    more clean up

--- scoutInformer/src/scout/ProgramDriver.java
+++ scoutInformer/src/scout/ProgramDriver.java
@@ -5,26 +5,6 @@ package scout;
  */
 public class ProgramDriver {
     public static void main(String args[]) {
-//        DBConnector.connectToDB();
-//
-//        try {
-//            Statement st = DBConnector.connection.createStatement();
-//            ResultSet rs = st.executeQuery("Select * FROM scout");
-//
-//            Scout scout = new Scout();
-//
-//            while (rs.next()) {
-//                scout.setFirstName(rs.getString(Scout.FIRST_NAME));
-//                scout.setLastName(rs.getString(Scout.LAST_NAME));
-//                scout.setSuffix(rs.getString(Scout.SUFFIX));
-//                scout.setAge(rs.getInt(Scout.AGE));
-//            }
-//        } catch (SQLException e) {
-//            System.err.println("not good from main");
-//            System.err.println(e.getMessage());
-//        }
-//        DBConnector.closeConnection();
-
         PnlMain pnlMain = new PnlMain();
         pnlMain.setVisible(true);
     }
