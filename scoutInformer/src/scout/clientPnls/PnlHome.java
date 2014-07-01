7d0005004893d1411c3bceb75cdb5d989ccc14a2
Malloch <nathanaelmalloch@yahoo.com>
2014-06-30 22:08:54 -0600

    added start of new page

---
+++ scoutInformer/src/scout/clientPnls/PnlHome.java
@@ -0,0 +1,35 @@
+/*
+ * Created by JFormDesigner on Mon Jun 30 21:50:13 MDT 2014
+ */
+
+package scout.clientPnls;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author User #2
+ */
+public class PnlHome extends JPanel {
+    public PnlHome() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+        setMinimumSize(new Dimension(1200, 800));
+        setPreferredSize(new Dimension(1200, 800));
+        setBackground(Color.white);
+        setLayout(new GridBagLayout());
+        ((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0, 0};
+        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0};
+        ((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 0.0, 1.0E-4};
+        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
