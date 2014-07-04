02974fe563965edabf1697f6f98de8b28b1bfa67
Malloch <nathanaelmalloch@yahoo.com>
2014-07-03 19:29:06 -0600

    fixed a border bug

--- scoutInformer/src/guiUtil/SelectionBorder.java
+++ scoutInformer/src/guiUtil/SelectionBorder.java
@@ -24,9 +24,9 @@ public class SelectionBorder extends AbstractBorder {
             // Top Border
             g2d.fill(new Rectangle2D.Double(x, y, width, 5));
             // Right Border
-            g2d.fill(new Rectangle2D.Double(width - 8, y, 5, height));
+            g2d.fill(new Rectangle2D.Double(width - 5, y, 7, height));
             // Bottom Border
-            g2d.fill(new Rectangle2D.Double(x, height - 8, width, 5));
+            g2d.fill(new Rectangle2D.Double(x, height - 5, width, 7));
             // Left Border
             if (gapTop > 5) {
                 g2d.fill(new Rectangle2D.Double(x, y, 5, gapTop));
