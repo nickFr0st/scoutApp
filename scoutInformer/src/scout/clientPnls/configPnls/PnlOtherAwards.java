8202c91f011ec016de415f9bfca592e439d6ab66
nickFrost <nathanaelmalloch@yahoo.com>
2014-12-06 11:24:00 -0700

    you can now delete multiple awards at once

--- scoutInformer/src/scout/clientPnls/configPnls/PnlOtherAwards.java
+++ scoutInformer/src/scout/clientPnls/configPnls/PnlOtherAwards.java
@@ -373,26 +373,15 @@ public class PnlOtherAwards extends JPanel implements Configuration {
 
         // (do this then scouts have been added to the program)
         // before deleting check to see if the award is used on any scouts
-
-        List<Integer> requirementIdList = new ArrayList<Integer>();
-        if (grid > 0) {
-            for (Component component : pnlRequirements.getComponents()) {
-                if (component instanceof PnlRequirement) {
-
-                    if (((PnlRequirement)component).getReqId() < 0) {
-                        continue;
-                    }
-
-                    requirementIdList.add(((PnlRequirement)component).getReqId());
-                }
-            }
-        }
-
         Util.processBusy(pnlBadgeConf.getBtnUpdate(), true);
-        LogicRequirement.deleteList(requirementIdList);
-        LogicOtherAward.delete(listAwardNames.getSelectedValue().toString());
-        Util.processBusy(pnlBadgeConf.getBtnUpdate(), false);
+        for (String advName : (List<String>) listAwardNames.getSelectedValuesList()) {
+            OtherAward advancement = LogicOtherAward.findByName(advName);
 
+            List<Integer> requirementIdList = LogicRequirement.findIdsByParentIdTypeId(advancement.getId(), RequirementTypeConst.OTHER.getId());
+            LogicRequirement.deleteList(requirementIdList);
+            LogicOtherAward.delete(advName);
+        }
+        Util.processBusy(pnlBadgeConf.getBtnUpdate(), false);
         clearData();
     }
 
